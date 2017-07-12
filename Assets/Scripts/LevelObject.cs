a48e278223139f080ade6b74dccfb8ec3b65724b
Steven Michael <juliusbox@hotmail.com>
2017-07-12 00:03:50 -0500

    Spinner progress

--- Assets/Scripts/LevelObject.cs
+++ Assets/Scripts/LevelObject.cs
@@ -7,7 +7,7 @@ public class LevelObject : MonoBehaviour {
 
 	public enum LevelObjectType{Box1, Set1Spinner, Plank, Spring, Coin, Wall, PlankJr, Set1WoodBlock, Set1Spring, Set1Plank, Set1Wall, Set1EndBox, 
 							    CoinPlus, Set1Spike, CameraAngle, ResetCamera, Set1MovingBlock, Set1MovingBlockReverse, Set1HalfPlank, Set1QuarterPlank,
-								Set2Plank1,Set2Plank2,Set2WoodBlock,Set2Spring,Set2DangerNail1,Set2DangerNail2,Set2SafeNail1,Set2SafeNail2 };
+								Set2Plank1,Set2Plank2,Set2WoodBlock,Set2Spring,Set2DangerNail1,Set2DangerNail2,Set2SafeNail1,Set2SafeNail2,Set2Spinner };
 	public LevelObjectType levelObjectType;
 	public bool isIntro;
 	public float forceTimeTillNext;
@@ -126,6 +126,9 @@ public class LevelObject : MonoBehaviour {
 			break;
 		case LevelObjectType.Set2SafeNail2:
 			return "Set2SafeNail2";
+			break;
+		case LevelObjectType.Set2Spinner:
+			return "Set2Spinner";
 			break;
 		default:
 			return "";
