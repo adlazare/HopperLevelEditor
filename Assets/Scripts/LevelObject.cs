e23fa4b50a2bad9d2cd75c9a9b50f712174a4429
Steven Michael <juliusbox@hotmail.com>
2017-07-13 00:05:54 -0500

    Spring 2

--- Assets/Scripts/LevelObject.cs
+++ Assets/Scripts/LevelObject.cs
@@ -7,7 +7,7 @@ public class LevelObject : MonoBehaviour {
 
 	public enum LevelObjectType{Box1, Set1Spinner, Plank, Spring, Coin, Wall, PlankJr, Set1WoodBlock, Set1Spring, Set1Plank, Set1Wall, Set1EndBox, 
 							    CoinPlus, Set1Spike, CameraAngle, ResetCamera, Set1MovingBlock, Set1MovingBlockReverse, Set1HalfPlank, Set1QuarterPlank,
-								Set2Plank1,Set2Plank2,Set2WoodBlock,Set2Spring,Set2DangerNail1,Set2DangerNail2,Set2SafeNail1,Set2SafeNail2,Set2Spinner,Set2Wall };
+								Set2Plank1,Set2Plank2,Set2WoodBlock,Set2Spring,Set2DangerNail1,Set2DangerNail2,Set2SafeNail1,Set2SafeNail2,Set2Spinner,Set2Wall,Set2Spring2 };
 	public LevelObjectType levelObjectType;
 	public bool isIntro;
 	public float forceTimeTillNext;
@@ -132,6 +132,9 @@ public class LevelObject : MonoBehaviour {
 			break;
 		case LevelObjectType.Set2Wall:
 			return "Set2Wall";
+			break;
+		case LevelObjectType.Set2Spring2:
+			return "Set2Spring2";
 			break;
 		default:
 			return "";
